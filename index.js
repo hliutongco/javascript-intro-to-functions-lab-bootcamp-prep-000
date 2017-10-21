402aae4f7070acecff377d2646aa9d0b1afe9b7b
Helen Liutongco <bellpickle@gmail.com>
2017-10-21 04:14:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,19 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 
+function sayHiToGrandma(string) {
+  if(string.toUpperCase()===string) {
+    return "YES INDEED!"
+  }
+  else if(string.toLowerCase()===string) {
+    return "I can't hear you!"
+  }
+  else if(string==="I love you, Grandma.") {
+    return "I love you, too.";
+  }
+  else {
+    return string;
+  }
+}
+
 logWhisper("Hello");
