fa50920e8485f4b145b230ee26cdfa8b33e8e722
Helen Liutongco <bellpickle@gmail.com>
2017-10-21 03:59:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ function shout(string) {
   return string.toUpperCase();
 }
 
+function whisper(string) {
+  return string.toLowerCase();
+}
+
 function logShout(string) {
   console.log(string.toUpperCase());
 }
 
-shout("Hello");
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+logWhisper("Hello");
